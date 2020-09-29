d0f58f511104e2a967db2e6dada4303a05fdd49f
swapnaneel bhuiya <bhuiya.swapnaneel@gmail.com>
2020-09-29 15:12:23 +0530

    UC7:At least one numeric character

--- src/main/java/UserRegMav/UserRegMain.java
+++ src/main/java/UserRegMav/UserRegMain.java
@@ -47,7 +47,7 @@ public class UserRegMain {
 	}
 
 	public boolean validPassword(String pass) {
-		if(pass.matches("[A-Z]+")&&pass.length()>=8)
+		if(pass.matches("[A-Z]+[0-9]+")&&pass.length()>=8)
 			return true;
 		else
 			return false;
